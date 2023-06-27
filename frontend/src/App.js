777b32832234410588d2869aa4e8cbd98642e760
r44ph <revaaph15@gmail.com>
2023-06-27 14:13:27 +0700

    6/27/23

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,19 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import './App.css';
 import Navbar from './components/Navbar.jsx'
-import Home from './container/Home'
+import Home from './pages/Home'
+import Join from './pages/Join'
 
 function App() {
   return (
     <div className="bg-[#0F1017] h-[655px]">
       <Navbar />
-      <Home />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Join />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
--- frontend/src/components/CTA.jsx
+++ frontend/src/components/CTA.jsx
@@ -1,9 +1,15 @@
+import { useNavigate } from 'react-router-dom'
 import React from 'react'
 
 const CTA = () => {
+  const navigate = useNavigate()
+
   return (
-    <div className="mt-[45px]">
-        <button className="flex justify-center items-center bg-[#E14817] w-[170px] h-[45px] rounded-[8px] text-white">Join a Meeting</button>
+    <div className="mt-[35px]">
+      <button
+      className="flex justify-center items-center bg-[#E14817] w-[170px] h-[45px] rounded-[8px] text-white"
+      onClick={() => navigate('/join')} 
+      >Join a Meeting</button>
     </div>
   )
 }
